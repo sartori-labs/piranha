91b7448596b7d9b3ffbb7eb07504e928f58b678e
Nishanth Somashekara Murthy <nishanthsmurthy24@gmail.com>
2025-12-15 14:00:35 -0600

    Fixed runtime library issue

--- scripts/piranha_setup.sh
+++ scripts/piranha_setup.sh
@@ -6,3 +6,6 @@ export LD_LIBRARY_PATH=${GOOGLETEST}/lib64:$LD_LIBRARY_PATH
 module load gcc/12.2.0
 module load cuda/12.3
 module load apps/cmake/3.29
+
+export LD_LIBRARY_PATH=/apps/common/gcc/12.2.0/lib64:$LD_LIBRARY_PATH
+
